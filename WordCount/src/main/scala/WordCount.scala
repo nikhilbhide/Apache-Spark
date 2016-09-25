078d709cf446d1787b97bb9f56f809aa851cc3a7
nikhilbhide <nikhilbhide15@gmail.com>
2016-09-25 13:14:01 -0700

    Initial Commit - Word Count

---
+++ WordCount/src/main/scala/WordCount.scala
@@ -0,0 +1,15 @@
+import org.apache.spark.SparkContext 
+import org.apache.spark.SparkContext._ 
+import org.apache.spark._  
+
+object WordCount { 
+   def main(args: Array[String]) { 
+      val sc = new SparkContext( "local", "Word Count", "/usr/local/spark", Nil, Map(), Map()) 
+		  val input = sc.textFile("in.txt") 
+      val count = input.flatMap(line ⇒ line.split(" ")) 
+      .map(word ⇒ (word, 1)) 
+      .reduceByKey((o1,o2)=>o1+o2)
+      count.saveAsTextFile("outfile2") 
+      System.out.println("OK"); 
+   } 
+} 
